98c3131570f264106ad4445a2bc85b08f2423433
Marin Desnica <marin.desnica@gmail.com>
2023-05-16 21:31:49 +0200

    add portfolio

--- src/components/page/Portfolio.tsx
+++ src/components/page/Portfolio.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from '../layout/Container';
 import { styled } from '@mui/system';
 import { Box, Typography, Link, Divider as MuiDivider } from '@mui/material';
-import { PROJECTS } from '../../const';
+import { PROJECTS, WEBSITES } from '../../const';
 
 const List = styled(Box)(({ theme }) => ({
   display: 'flex',
@@ -31,31 +31,63 @@ export const Portfolio: React.FC = () => {
     <Container>
       <Typography variant='h5'>Welcome to My Coding Playground!</Typography>
       <List>
-        {PROJECTS.map((project) => (
-          <Item key={project.githubLink}>
+        {WEBSITES.map((website) => (
+          <Item key={website.url}>
+            <Typography variant='h6'>{website.title}</Typography>
+            <Typography variant='subtitle1'>{website.summary}</Typography>
+            <Box>
+              <Label variant='body1'>Link:</Label>
+              <Link
+                href={website.url}
+                target='_blank'
+                rel='noopener noreferrer'
+                color='text.secondary'
+              >
+                {website.url}
+              </Link>
+            </Box>
+            {website.github && (
+              <Box>
+                <Label variant='body1'>Project:</Label>
+                <Link
+                  href={website.github}
+                  rel='noopener noreferrer'
+                  color='text.secondary'
+                >
+                  {website.github}
+                </Link>
+              </Box>
+            )}
+            <Divider light />
+          </Item>
+        ))}
+      </List>
+      <List>
+        {PROJECTS.map((project, i) => (
+          <Item key={project.url}>
             <Typography variant='h6'>{project.title}</Typography>
             <Typography variant='subtitle1'>{project.summary}</Typography>
             <Box>
               <Label variant='body1'>Demo:</Label>
               <Link
-                href={project.demoLink}
+                href={project.url}
                 rel='noopener noreferrer'
                 color='text.secondary'
               >
-                {project.demoLink}
+                {project.url}
               </Link>
             </Box>
             <Box>
-              <Label variant='body1'>Project:</Label>
+              <Label variant='body1'>Source code:</Label>
               <Link
-                href={project.githubLink}
+                href={project.github}
                 rel='noopener noreferrer'
                 color='text.secondary'
               >
-                {project.githubLink}
+                {project.github}
               </Link>
             </Box>
-            <Divider light />
+            {i + 1 < PROJECTS.length && <Divider light />}
           </Item>
         ))}
       </List>
--- src/const.ts
+++ src/const.ts
@@ -5,26 +5,35 @@ export const NAME = 'Marin Desnica';
 export const APP_NAME = `<Des />`;
 export const WEBSITE = 'https://mardesnic.github.io/';
 
+export const WEBSITES = [
+  {
+    title: 'Entasis',
+    summary: 'Website for an architectural design studio from Dubrovnik. ',
+    url: 'https://entasis-du.hr/',
+    github: '',
+  },
+];
+
 export const PROJECTS = [
   {
     title: '<Trivia />',
     summary:
       'Trivia is a fun and engaging trivia app built with React and MUI Material. ',
-    demoLink: 'https://mardesnic.github.io/des-trivia/',
-    githubLink: 'https://github.com/mardesnic/des-trivia',
+    url: 'https://mardesnic.github.io/des-trivia/',
+    github: 'https://github.com/mardesnic/des-trivia',
   },
   {
     title: '<Flappy />',
     summary:
       'Flappy is a clone of the popular game Flappy Bird created with React.',
-    demoLink: 'https://mardesnic.github.io/des-flappy/',
-    githubLink: 'https://github.com/mardesnic/des-flappy',
+    url: 'https://mardesnic.github.io/des-flappy/',
+    github: 'https://github.com/mardesnic/des-flappy',
   },
   {
     title: '<CatanMapBuilder />',
     summary:
       'Simple web app for generating Catan maps built with React and MUI Material.',
-    demoLink: 'https://mardesnic.github.io/des-catan-map-builder/',
-    githubLink: 'https://github.com/mardesnic/des-catan-map-builder',
+    url: 'https://mardesnic.github.io/des-catan-map-builder/',
+    github: 'https://github.com/mardesnic/des-catan-map-builder',
   },
 ];
